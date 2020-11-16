6710afa5c4c763dfacf78a0095448352ce78a393
ohanabene <54714245+ohanabene@users.noreply.github.com>
2020-11-15 21:39:35 -0600

    Add files via upload

--- trapPlot.C
+++ trapPlot.C
@@ -11,129 +11,100 @@
 
 void trapPlot() {
     
+    double e=1.6e-19;
+    double r_0 = 0.03;
+    double m= 2.1801714e-25;
     
-    //Defining some constants
-    double e= 1.6e-19;
-    double r_0 = 3e-3;
-    double m= 2.28e-25;
-
-    double drag = 3.47e-16;
     
-    //Creating my root file
-    TFile *inputFile = new TFile("trapOutput_precious_withdrag.root");
+    TFile *inputFile = new TFile("trapOutput_vacuum_e7.root");
+    
     TTree *trapTree;
     trapTree = (TTree*)inputFile->Get("trap");
     
-    //Setting my variables and creating the branchs for them
     std::vector<double> *vector_normR = 0;
     std::vector<double> *vector_normZ = 0;
     std::vector<double> *vector_Vac = 0;
     std::vector<double> *vector_Vdc = 0;
-    std::vector<double> *vector_frequency = 0;
-    std::vector<double> *vector_avgV = 0;
+    std::vector<double> *vector_w = 0;
     
     trapTree->SetBranchAddress("norm_R", &vector_normR);
     trapTree->SetBranchAddress("norm_Z", &vector_normZ);
     trapTree->SetBranchAddress("Vdc", &vector_Vdc);
     trapTree->SetBranchAddress("Vac", &vector_Vac);
-    trapTree->SetBranchAddress("w", &vector_frequency);
-    trapTree->SetBranchAddress("vAvg", &vector_avgV);
+    trapTree->SetBranchAddress("w", &vector_w);
+    
+    
     trapTree->GetEntry(0);
     
+    TGraph *gr = new TGraph();
+    TGraph *gz = new TGraph();
+    TGraph *gboth = new TGraph();
+    TH1F *stable_Vac = new TH1F("stable_Vac","Vac",20 ,0 ,500);
+    TH1F *stable_Vdc = new TH1F("stable_Vdc","Vdc",100 ,-150 ,150);
+    TH1F *stable_w = new TH1F("stable_w","w",999 ,3000 ,3000000);
     
-    //Creating the 3D histogram for the stability bands
-    TH3* h3 = new TH3D("h3", "Stability plot", 200, -10.0, 10.0, 200, -10.0, 10.0,
-                       100, 0.0, 1.0);
+    TMultiGraph *multi = new TMultiGraph();
     
-    //##############################################//
-    //######### Old block of code , ignore #########//
-    //##############################################//
-    
-    // TGraph *gr = new TGraph();
-    //TGraph *gz = new TGraph();
-    //TGraph *gboth = new TGraph();
-    //TMultiGraph *multi = new TMultiGraph();
-    
-    //##############################################//
-    //##############################################//
-    //##############################################//
-    
-    //Looping over all the elements in my TTree and retrieving the information from them
     for (int i = 0; i < vector_Vac->size(); i++) {
         
         double r_norm = vector_normR->at(i);
         double z_norm = vector_normZ->at(i);
         double thisVac = vector_Vac->at(i);
         double thisVdc = vector_Vdc->at(i);
-        double thisW = vector_frequency->at(i);
-        double thisvAvg = vector_avgV->at(i);
+        double thisW = vector_w->at(i);
+        double a = (4*e*thisVdc)/(m*r_0 *r_0 * thisW*thisW);
+        double q = (2*e*thisVac)/(m*r_0 *r_0 * thisW*thisW);
         
-        //Calculating my dimensionless variables
-        double a = (-16*e*thisVdc)/(m*r_0 *r_0 * thisW*thisW);
-        double q = (8*e*thisVac)/(m*r_0 *r_0 * thisW*thisW);
-        double vTerm = m*g/drag;
+        if(r_norm < 3){
+            
+            gr->SetPoint(i, q, a);
+        }
         
+        if(z_norm < 3){
+            
+            gz->SetPoint(i, q, a);
+            
+        }
         
-        //This variable is 1 if the particle is in a unstable setting and 0 if it is perfectly stable
-        double x = thisvAvg/vTerm;
-        
-        h3->Fill(a,q,x);
-        
-        
-        //##############################################//
-        //######### Old block of code , ignore #########//
-        //##############################################//
-        /*  if(r_norm < (r_0)){
-         
-         gr->SetPoint(i, q, a);
-         
-         }
-         
-         if(z_norm < (r_0)){
-         
-         gz->SetPoint(i, q, a);
-         
-         }
-         
-         if((r_norm < (r_0)) && (z_norm < (r_0))){
-         
-         gboth->SetPoint(i, q, -a);
-         
-         }*/
-        //##############################################//
-        //##############################################//
-        //##############################################//
-        
-    } // -> closing the loop
+        if((r_norm < 3) && (z_norm < 3)){
+            
+            gboth->SetPoint(i, q, -a);
+            stable_Vac->Fill(thisVac);
+            stable_Vdc->Fill(thisVdc);
+            stable_w->Fill(thisW);
+        }
+    }
     
-    //Drawing my histogram
-    h3->Draw();
-    TCanvas *c1 = new TCanvas("c1", "c1", 700, 500);
-    
-    //##############################################//
-    //######### Old block of code , ignore #########//
-    //##############################################//
-    // multi->Add(gz);
-    //multi->Add(gr);
-    //multi->Add(gboth);
-    //multi->GetXaxis()->SetTitle("q");
-    //multi->GetYaxis()->SetTitle("a");
-    //multi->Draw("AP");
-    //c1->RedrawAxis();
+    TCanvas *c1 = new TCanvas("c1", "c1", 0, 300);
     /*
+    stable_w->Draw();
+    
+    TCanvas *c2 = new TCanvas("c2", "c2", 0, 500);
+    stable_Vac->Draw();
+    
+    TCanvas *c3 = new TCanvas("c3", "c3", -150, 150);
+    stable_Vdc->Draw();
+    */
+     //multi->Add(gz);
+     //multi->Add(gr);
+     multi->Add(gboth);
+     multi->GetXaxis()->SetTitle("q");
+     multi->GetYaxis()->SetTitle("a");
+     multi->Draw("AP");
+     c1->RedrawAxis();
+     
+     /*
      gz->SetMarkerColor(5);
      gz->SetMarkerStyle(3);
      
      gr->SetMarkerColor(6);
      gr->SetMarkerStyle(3);
-     
-     gboth->SetMarkerColor(9);
+     */
+     gboth->SetMarkerColor(7);
      gboth->SetMarkerStyle(3);
      
-     */
-    //##############################################//
-    //##############################################//
-    //##############################################//
+     
+    
     
 }
 
